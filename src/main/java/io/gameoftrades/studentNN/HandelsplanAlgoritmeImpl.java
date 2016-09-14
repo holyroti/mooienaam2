4c80d164d076ada06940d8bc479fc430a46bc02c
MrCtje <c.daveiga@live.nl>
2016-09-14 17:53:46 +0200

    Opdracht 4 classes

--- src/main/java/io/gameoftrades/studentNN/HandelsplanAlgoritmeImpl.java
+++ src/main/java/io/gameoftrades/studentNN/HandelsplanAlgoritmeImpl.java
@@ -7,8 +7,14 @@ package io.gameoftrades.studentNN;
 
 import io.gameoftrades.model.Wereld;
 import io.gameoftrades.model.algoritme.HandelsplanAlgoritme;
+import io.gameoftrades.model.kaart.Stad;
+import io.gameoftrades.model.markt.Handel;
+import io.gameoftrades.model.markt.HandelType;
 import io.gameoftrades.model.markt.Handelsplan;
+import io.gameoftrades.model.markt.Handelswaar;
 import io.gameoftrades.model.markt.actie.HandelsPositie;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  *
@@ -23,9 +29,26 @@ public class HandelsplanAlgoritmeImpl implements HandelsplanAlgoritme{
         int capacity = hp.getRuimte();
         int timeLeft = hp.getTotaalActie();
         int winst = hp.getTotaalWinst();
+        Stad beginStad = hp.getStad();
         
+        ArrayList<Integer> winstList = new ArrayList<>();
         
-        return null;
+        for(Handel handel: wereld.getMarkt().getHandel()){
+            if(handel.getHandelType().equals(HandelType.BIEDT))
+                
+                
+        }
+//        for(Handel handel: wereld.getMarkt().getHandel()){
+//            System.out.println(handel.getPrijs());
+//        }
+        System.out.println(wereld.getMarkt().getHandel().get(0).getHandelswaar());
+        return new Handelsplan(null);
     }
+
+    @Override
+    public String toString() {
+        return "Handelsplan algorithme";
+    }
+    
     
 }
